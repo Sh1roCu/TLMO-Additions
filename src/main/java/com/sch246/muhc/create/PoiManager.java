50f13a1e125add4bcf8d7464fa10ac96a3a4f790
mikasa <1244456115@qq.com>
2026-03-21 17:15:24 +0800

    feat: support `Create: Connected Fabric` for `MaidUseHandCrack`

--- src/main/java/com/sch246/muhc/create/PoiManager.java
+++ src/main/java/com/sch246/muhc/create/PoiManager.java
@@ -1,6 +1,7 @@
 package com.sch246.muhc.create;
 
 import com.google.common.collect.ImmutableSet;
+import com.hlysine.create_connected.CCBlocks;
 import com.simibubi.create.AllBlocks;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
@@ -31,15 +32,14 @@ public final class PoiManager {
 
         // 直接引用，如果mod未安装，下面代码不会被调用（但编译仍需依赖，需加 optional 依赖）
         if (isCreateConnectedLoaded()) {
-            // TODO: wait for Fabric port
-//            crankWheelStates = CCBlocks.CRANK_WHEEL.get()
-//                    .getStateDefinition()
-//                    .getPossibleStates()
-//                    .stream();
-//            largeCrankWheelStates = CCBlocks.LARGE_CRANK_WHEEL.get()
-//                    .getStateDefinition()
-//                    .getPossibleStates()
-//                    .stream();
+            crankWheelStates = CCBlocks.CRANK_WHEEL.get()
+                    .getStateDefinition()
+                    .getPossibleStates()
+                    .stream();
+            largeCrankWheelStates = CCBlocks.LARGE_CRANK_WHEEL.get()
+                    .getStateDefinition()
+                    .getPossibleStates()
+                    .stream();
         }
 
         // 合并所有，收集成 Set
